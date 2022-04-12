fa8d1dd1206bc1a1bc5c37ffa4ecd0186fa0c932
Fábrica de Software CEET <vladimir.cortes@outlook.com>
2022-04-12 16:09:31 -0500

    Recurso USUARIOS expuesto a traves de un endpoint de tipo POST

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -7,6 +7,55 @@ ruta.get('/', (req,res)=>{
     res.json('Respuesta a petición GET de USUARIOS funcionando correctamente...');
 });
 
+// Validacones para el objeto usuario
+const schema = Joi.object({
+    nombre: Joi.string()
+        .min(3)
+        .max(30)
+        .required()
+        .pattern(/^[A-Za-záéíóú ]{3,30}$/),
 
+    password: Joi.string()
+        .pattern(/^[a-zA-Z0-9]{3,30}$/),
+
+    email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'edu', 'co'] } })
+});
+
+
+
+// Endpoint de tipo POST para el recurso USUARIOS
+ruta.post('/', (req, res) => {
+    let body = req.body;
+
+    const {error, value} = schema.validate({nombre: body.nombre, email: body.email});
+    if(!error){
+        let resultado = crearUsuario(body);
+
+        resultado.then( user => {
+            res.json({
+                valor: user
+            })
+        }).catch( err => {
+            res.status(400).json({
+                err
+            })
+        });
+    }else{
+        res.status(400).json({
+            error
+        })
+    }    
+});
+
+// Función asíncrona para crear un objeto de tipo usuario
+async function crearUsuario(body){
+    let usuario = new Usuario({
+        email       : body.email,
+        nombre      : body.nombre,
+        password    : body.password
+    });
+    return await usuario.save();
+}
 
 module.exports = ruta;
