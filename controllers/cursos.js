83a8a862004229941da976ad26521946622b079b
Fábrica de Software CEET <vladimir.cortes@outlook.com>
2022-04-12 23:19:47 -0500

    Agregando y probando la funcionalidad para crear cursos. Endpoint de tipo POST

--- controllers/cursos.js
+++ controllers/cursos.js
@@ -6,4 +6,32 @@ ruta.get('/', (req,res)=>{
     res.json('Respuesta a petición GET de CURSOS funcionando correctamente...');
 });
 
+
+
+// Endpoint de tipo POST para el recurso CURSOS
+ruta.post('/', (req, res) => {
+    let resultado = crearCurso(req.body);
+
+    resultado.then(curso => {
+        res.json({
+            curso
+        })
+    }).catch(err => {
+        res.status(400).json({
+            err
+        })
+    })
+});
+
+// Función asíncrona para crear cursos
+async function crearCurso(body){
+    let curso = new Curso({
+        titulo       : body.titulo,
+        descripcion  : body.descripcion,
+        alumnos  : body.alumnos,
+        calificacion : body.calificacion
+    });
+    return await curso.save();
+}
+
 module.exports = ruta;
