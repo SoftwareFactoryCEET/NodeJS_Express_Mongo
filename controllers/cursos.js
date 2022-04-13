daee112aebddd5d385ac819ba9fb13a4ae8b9823
Fábrica de Software CEET <vladimir.cortes@outlook.com>
2022-04-13 16:21:15 -0500

    Agregando y probando la funcionalidad para eliminar (inactivar)  cursos. Endpoint de tipo DELETE

--- controllers/cursos.js
+++ controllers/cursos.js
@@ -33,6 +33,16 @@ ruta.put('/:id', (req, res) => {
     })
 })
 
+// Endpoint de tipo DELETE para el recurso CURSOS
+ruta.delete('/:id', (req, res) => {
+    let resultado = desactivarCurso(req.params.id);
+    resultado.then(curso => {
+        res.json(curso);
+    }).catch(err => {
+        res.status(400).json(err);
+    })
+})
+
 // Función asíncrona para crear cursos
 async function crearCurso(body){
     let curso = new Curso({
@@ -52,6 +62,16 @@ async function actualizarCurso(id, body){
             descripcion: body.descripcion,
             alumnos  : body.alumnos,
             calificacion : body.calificacion
+        }
+    }, {new: true});
+    return curso;
+}
+
+// Función asíncrona para inactivar cursos
+async function desactivarCurso(id){
+    let curso = await Curso.findByIdAndUpdate(id, {
+        $set: {
+            estado: false
         }
     }, {new: true});
     return curso;
